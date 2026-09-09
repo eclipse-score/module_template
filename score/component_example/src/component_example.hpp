d643fbaf110517b413b042961fa24148a788526d
Naveena Panne <naveena.panne@bosch.com>
2026-09-09 10:28:31 +0530

    Added code and test for example component (#169)
    * Added code and test for example component
    
    * Fix document CI failure
    
    Signed-off-by: Naveena Panne <pnn9kor@bosch.com>
    
    * Fix document CI failure
    
    Signed-off-by: Naveena Panne <pnn9kor@bosch.com>
    
    ---------
    
    Signed-off-by: Naveena Panne <naveenbellare@gmail.com>
    Signed-off-by: Naveena Panne <pnn9kor@bosch.com>
    Co-authored-by: Naveena Panne <pnn9kor@bosch.com>

---
+++ score/component_example/src/component_example.hpp
@@ -0,0 +1,27 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+#ifndef SCORE_COMPONENT_EXAMPLE_COMPONENT_EXAMPLE_HPP_
+#define SCORE_COMPONENT_EXAMPLE_COMPONENT_EXAMPLE_HPP_
+
+#include <string>
+
+namespace score {
+namespace component_example {
+
+std::string make_hello_message(const std::string& name);
+
+}  // namespace component_example
+}  // namespace score
+
+#endif  // SCORE_COMPONENT_EXAMPLE_COMPONENT_EXAMPLE_HPP_
